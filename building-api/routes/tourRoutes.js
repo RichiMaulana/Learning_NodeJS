861952c7e6f664539216561702e5a4527ad10c27
RichiMaulana <richimaulana30@gmail.com>
2022-12-16 08:27:30 +0700

    add new route, top 5 tours

--- building-api/routes/tourRoutes.js
+++ building-api/routes/tourRoutes.js
@@ -7,12 +7,16 @@ const tourRouter = express.Router();
 // Param in middleware
 // Can be used to check the ID first, so the endpoint that needs the id is always get the id, and to prevent re write or use the same code every time it needed
 // AKA used to validation the ID
-tourRouter.param('uuid', tourController.checkId);
+// tourRouter.param('uuid', tourController.checkId);
+
+tourRouter
+  .route('/top-5-tours')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 tourRouter
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 tourRouter
   .route('/:uuid')
