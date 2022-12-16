2d8d3e9ade25e63e8f9399afe1aa6e630e1fa47c
RichiMaulana <richimaulana30@gmail.com>
2022-12-16 08:30:07 +0700

    adjust toursController
    add some query features

--- building-api/controllers/toursController.js
+++ building-api/controllers/toursController.js
@@ -2,6 +2,13 @@
 
 const { toursModel } = require('../models');
 
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.page = '1';
+  req.query.sort = 'ratingsAverage';
+  next();
+};
+
 exports.checkId = async (req, res, next, val) => {
   const checking = await toursModel.findOne({ where: { uuid: val } });
 
@@ -14,29 +21,83 @@ exports.checkId = async (req, res, next, val) => {
   next();
 };
 
-exports.checkBody = (req, res, next) => {
-  // const data = req.body;
+// exports.checkBody = (req, res, next) => {
+//   // const data = req.body;
 
-  // if (!data.name || !data.price) {
-  //   return res.status(400).json({
-  //     status: 'Bad Request',
-  //     message:
-  //       'Request body does not meet the minimum requirements, Name or Price',
-  //   });
-  // }
-  next();
-};
+//   // if (!data.name || !data.price) {
+//   //   return res.status(400).json({
+//   //     status: 'Bad Request',
+//   //     message:
+//   //       'Request body does not meet the minimum requirements, Name or Price',
+//   //   });
+//   // }
+//   next();
+// };
 
 exports.getAllTours = async (req, res) => {
-  const tours = await toursModel.findAll();
+  try {
+    const queryObj = { ...req.query };
+    const excludedQueryObj = [
+      'page',
+      'sort',
+      'limit',
+      'fields',
+      'order',
+      'size',
+    ];
+    excludedQueryObj.forEach((el) => delete queryObj[el]);
 
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
-  });
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\bgte|gt|lte|lt\b/g, (match) => `$${match}`);
+
+    ///
+
+    let attributes;
+    if (req.query.fields) {
+      attributes = req.query.fields.split(',');
+    }
+
+    const { page } = req.query || 1;
+    const { limit } = req.query || 100;
+    const skip = Math.max(page - 1, 0) * limit;
+
+    if (req.query.page) {
+      const toursCount = await toursModel.count();
+      if (skip >= toursCount) throw new Error('This page does not exist');
+    }
+
+    const query = {
+      limit: limit,
+      where: JSON.parse(queryStr),
+      offset: skip,
+      order: [[req.query.sort, req.query.order || 'DESC']],
+      attributes,
+    };
+
+    // const query = new APIFeature(req.query).query();
+    // console.log(req.query);
+
+    const tours = await toursModel.findAll(query);
+    // const tours = await toursModel.findAll({
+    //   where: queryObj,
+    //   limit: req.query.limit,
+    //   offset: req.query.page,
+    // });
+
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: err.message,
+    });
+  }
 };
 
 exports.getTour = async (req, res) => {
@@ -60,38 +121,8 @@ exports.getTour = async (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  const {
-    name,
-    duration,
-    description,
-    maxGroupSize,
-    difficulty,
-    ratingsAverage,
-    ratingsQuantity,
-    price,
-    summary,
-    imageCover,
-    images,
-    startDates,
-  } = req.body;
-
   try {
-    const tour = await toursModel.create({
-      name,
-      duration,
-      description,
-      maxGroupSize,
-      difficulty,
-      ratingsAverage,
-      ratingsQuantity,
-      price,
-      summary,
-      imageCover,
-      images,
-      startDates,
-    });
-
-    console.log(tour);
+    const tour = await toursModel.create(req.body);
 
     res.status(201).json({
       status: 'Success',
@@ -108,45 +139,27 @@ exports.createTour = async (req, res) => {
 };
 
 exports.updateTour = async (req, res) => {
-  const { uuid } = req.params;
+  const uuid = req.params;
 
-  const {
-    name,
-    duration,
-    description,
-    maxGroupSize,
-    difficulty,
-    ratingsAverage,
-    ratingsQuantity,
-    price,
-    summary,
-    imageCover,
-    images,
-    startDates,
-  } = req.body;
+  // const {
+  //   name,
+  //   duration,
+  //   description,
+  //   maxGroupSize,
+  //   difficulty,
+  //   ratingsAverage,
+  //   ratingsQuantity,
+  //   price,
+  //   summary,
+  //   imageCover,
+  //   images,
+  //   startDates,
+  // } = req.body;
 
   try {
-    await toursModel.update(
-      {
-        name,
-        duration,
-        description,
-        maxGroupSize,
-        difficulty,
-        ratingsAverage,
-        ratingsQuantity,
-        price,
-        summary,
-        imageCover,
-        images,
-        startDates,
-      },
-      {
-        where: {
-          uuid: uuid,
-        },
-      }
-    );
+    await toursModel.update(req.body, {
+      where: uuid,
+    });
 
     res.status(200).json({
       status: 'Success',
