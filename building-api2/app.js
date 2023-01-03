e2cbf8b7d66836155a03f658def14f7fc874d793
RichiMaulana <richimaulana30@gmail.com>
2023-01-03 14:13:21 +0700

    passport oauth

--- building-api2/app.js
+++ building-api2/app.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
-const mainRouter = require("./src/routes");
+const session = require("express-session");
 
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const mainRouter = require("./src/routes");
+const { sequelize } = require("./src/models");
+const authRoute = require("./src/routes/authRoute");
+const passport = require("./src/services/passport");
+
+const myStore = new SequelizeStore({
+  db: sequelize,
+});
+
+app.use(
+  session({
+    secret: "keryboard cat",
+    store: myStore,
+    resave: false,
+    proxy: false,
+    saveUninitialized: false,
+  })
+);
+
+myStore.sync();
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 
+app.use("/api/auth/", authRoute);
 app.use("/api/v1/", mainRouter);
 
 module.exports = app;
--- building-api2/src/routes/index.js
+++ building-api2/src/routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userRoutes = require("./userRoutes");
 const postRoutes = require("./postRoutes");
-const userController = require("../controller/userController");
 
 router.route("/").get((req, res) => {
   res.send(req.body);
